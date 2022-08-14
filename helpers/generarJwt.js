a887543a2bccd7bf2a837bb10b5db504b1f6b6cb
Juan Ovilla <cjuan2921@gmail.com>
2022-08-14 17:43:35 -0500

    Correccion de generarJWT

--- helpers/generarJwt.js
+++ helpers/generarJwt.js
@@ -1,27 +1,32 @@
 const jwt = require('jsonwebtoken');
 
-const generarJWT = (uid) => {
 
-    return new Promise((resolve, reject) => {
-        
-        const payload = {uid};
 
-        jwt.sign(payload, process.env.SECRETORPRIVATEKEY, {
+const generarJWT = ( uid = '' ) => {
+
+    return new Promise( (resolve, reject) => {
+
+        const payload = { uid };
+
+        jwt.sign( payload, process.env.SECRETORPRIVATEKEY, {
             expiresIn: '4h'
-        }, (err, token) => {
-            if (err) {
+        }, ( err, token ) => {
+
+            if ( err ) {
                 console.log(err);
-                reject('No se pudo generar el token');
+                reject( 'No se pudo generar el token' )
             } else {
-                resolve(token);
+                resolve( token );
             }
-        });
+        })
 
-    });
-
+    })
 }
+
+
 
 
 module.exports = {
     generarJWT
 }
+
