821a94bd567c8827b5738838b032eedf38deb9a8
Juan Ovilla <cjuan2921@gmail.com>
2022-08-25 15:28:57 -0500

    Uploads Cloudinary ADDED

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const fs = require('fs');
 
+const cloudinary = require('cloudinary').v2;
+cloudinary.config(process.env.CLOUDINARY_URL);
+
 const {response} = require('express');
 const { subirArchivo } = require('../helpers');
 
@@ -63,6 +66,51 @@ const actualizarImagen = async(req, res = response) => {
     res.json(modelo);
 }
 
+const actualizarImagenCloudinary = async(req, res = response) => {
+
+    const {id, coleccion} = req.params;
+    
+    let modelo;
+
+    switch (coleccion) {
+        case 'usuarios':
+            modelo = await Usuarios.findById(id);
+            if (!modelo) {
+                return res.status(400).json({
+                    msg: `No existe un usuario con el id ${id}`
+                });
+            }
+        break;
+        case 'tour':
+            modelo = await Tour.findById(id);
+            if (!modelo) {
+                return res.status(400).json({
+                    msg: `No existe un tour con el id ${id}`
+                });
+            }
+        break;
+        default:
+            return res.status(500).json({msg: 'Falta validar esto'});
+    }
+
+    // Limpiar imagenes previas
+    if (modelo.img) {
+        const nombreArr = modelo.img.split('/');
+        const nombre = nombreArr[nombreArr.length - 1];
+        const [public_id] = nombre.split('.');
+        cloudinary.uploader.destroy(public_id);
+    }
+
+    const {tempFilePath} = req.files.archivo;
+    const {secure_url} = await cloudinary.uploader.upload(tempFilePath);
+    modelo.img = secure_url;
+
+    await modelo.save();
+
+
+    res.json(modelo);
+}
+
 const mostrarImagen = async(req, res = response) => {
     
     const {id, coleccion} = req.params;
@@ -105,5 +153,6 @@ const mostrarImagen = async(req, res = response) => {
 module.exports = {
     cargarArchivo,
     actualizarImagen,
-    mostrarImagen
+    mostrarImagen,
+    actualizarImagenCloudinary
 }
--- routes/uploads.js
+++ routes/uploads.js
@@ -2,7 +2,7 @@ const {Router} = require('express');
 const {check} = require('express-validator');
 
 const {validarCampos, validarArchivoSubir} = require('../middleware');
-const { cargarArchivo, actualizarImagen, mostrarImagen } = require('../controllers/uploads');
+const { cargarArchivo, mostrarImagen, actualizarImagenCloudinary } = require('../controllers/uploads');
 const { coleccionesPermitidas } = require('../helpers');
 
 const router = Router();
@@ -15,7 +15,8 @@ router.put('/:coleccion/:id', [
     check('id', 'El id debe ser valido de mongo').isMongoId(),
     check('coleccion').custom(c => coleccionesPermitidas(c, ['usuarios', 'tour'])),
     validarCampos
-],actualizarImagen);
+],actualizarImagenCloudinary);
+// actualizarImagen
 
 // Servir las imagenes
 router.get('/:coleccion/:id', [
